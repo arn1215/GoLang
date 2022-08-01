a142fc3419b0c922b0b676060671ba5dba430f8d
Ali <ali.naqvi.dev@gmail.com>
2022-08-01 18:10:49 -0500

    fmt

--- main.go
+++ main.go
@@ -5,9 +5,17 @@ import (
 )
 
 func main() {
+	var name string
+	var age uint
+
 	fmt.Println("What's your name?")
-	var resp string
-	fmt.Scan(&resp)
-	fmt.Printf("My name is %v", resp)
+	fmt.Scan(&name)
+	fmt.Printf("Hello %v", name)
+	fmt.Println("What is your age?")
+	fmt.Scan(&age)
+	fmt.Printf("Nice to meet you %s, I'm also %d years old.", name, age)
+
+	newMessage := fmt.Sprintf("yo %d years is a good age to be, %s", age, name)
+	fmt.Println(newMessage)
 
 }
